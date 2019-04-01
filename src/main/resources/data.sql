a01a066c6561b7cd127bc9660d19997a6546946b
ggomi <dhkdn4042@naver.com>
2019-04-01 18:34:42 +0900

    특정 place 추가하는 query

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO `events` (`id`, `created_at`, `updated_at`)
+-- VALUES
+-- 	('240a0658-dc5f-4878-9381-ebb7b2667772', '2019-03-30 12:12:12', '2019-03-30 12:12:12');
+-- --
+-- --
+-- INSERT INTO `events` (`id`, `created_at`, `updated_at`, `review_id`)
+-- VALUES
+-- 	('123a0658-dc5f-4878-9381-ebb7b2667772', '2019-03-30 12:12:12', '2019-03-30 12:12:12', '240a0658-dc5f-4878-9381-ebb7b2667772');
+
+INSERT INTO `place` (`id`,`created_at`,`updated_at`,`value`)
+VALUES
+('2e4baf1c-5acb-4efb-a1af-eddada31b00f','2019-03-30 12:12:12','2019-03-30 12:12:12',null);
+
+INSERT INTO `place` (`id`,`created_at`,`updated_at`,`value`)
+VALUES
+('3dv5t1sc-5acb-4efb-a1af-2345firgnikl','2019-03-30 12:12:12','2019-03-30 12:12:12',null);
+
