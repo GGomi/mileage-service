8c3da49529c87b4e534f576e5d9a8de203e5afe3
ggomi <dhkdn4042@naver.com>
2019-04-01 19:25:44 +0900

    GetPointResponse 추가

---
+++ src/main/java/com/essri/mileage/point/dto/GetPointResponse.java
@@ -0,0 +1,34 @@
+package com.essri.mileage.point.dto;
+
+import com.essri.mileage.event.dto.EventActionResponse;
+import com.essri.mileage.event.model.Event;
+import com.essri.mileage.history.History;
+import com.essri.mileage.point.Points;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class GetPointResponse {
+    private String userId;
+    private long point;
+    private List<EventActionResponse> histories;
+
+    public GetPointResponse(Points point) {
+        this.userId = point.getId();
+        this.point = point.getPoint();
+        this.histories = buildHistory(point.getHistories());
+    }
+
+    public List<EventActionResponse> buildHistory(List<History> histories) {
+        List<EventActionResponse> returns = new ArrayList<>();
+
+        for (History history : histories) {
+            Event event = history.getEvents();
+            returns.add(new EventActionResponse(event));
+        }
+
+        return returns;
+    }
+}
