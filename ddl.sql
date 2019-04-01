d71a2402fd772410afe9218fe2ee1c91c5832d9e
ggomi <dhkdn4042@naver.com>
2019-04-01 19:26:48 +0900

    Table DDL 추가

---
+++ ddl.sql
@@ -0,0 +1,52 @@
+CREATE TABLE `events` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `action` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `photos` varchar(255) NOT NULL,
+  `place_id` varchar(255) NOT NULL,
+  `point` bigint(20) NOT NULL,
+  `review_id` varchar(255) DEFAULT NULL,
+  `type` varchar(255) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `place` (
+  `id` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `point_history` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `events_id` bigint(20) NOT NULL,
+  `points_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKbich1o0xc7kaek9ads6eick3m` (`events_id`),
+  KEY `FKrj6q3b4yrsrck6rqbcqfv9x13` (`points_id`),
+  CONSTRAINT `FKbich1o0xc7kaek9ads6eick3m` FOREIGN KEY (`events_id`) REFERENCES `events` (`id`),
+  CONSTRAINT `FKrj6q3b4yrsrck6rqbcqfv9x13` FOREIGN KEY (`points_id`) REFERENCES `points` (`id`)
+)
+
+CREATE TABLE `points` (
+  `id` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `point` bigint(20) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+CREATE TABLE `review` (
+  `id` varchar(255) NOT NULL,
+  `content` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `photos` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
