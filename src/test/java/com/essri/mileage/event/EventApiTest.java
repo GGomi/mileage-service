3482d4bd55c06ac3167f98ca5f84cb0fc8f9e1c3
ggomi <dhkdn4042@naver.com>
2019-04-04 21:06:50 +0900

    Add travis

--- src/test/java/com/essri/mileage/event/EventApiTest.java
+++ src/test/java/com/essri/mileage/event/EventApiTest.java
@@ -1,74 +1,79 @@
 package com.essri.mileage.event;
 
 import com.essri.mileage.event.domain.ActionType;
-import com.essri.mileage.event.domain.Events;
 import com.essri.mileage.event.domain.ReviewType;
 import com.essri.mileage.event.dto.EventActionRequest;
 import com.essri.mileage.event.service.EventAddService;
-import com.essri.mileage.exception.ExceptionApi;
-import com.essri.mileage.review.ReviewRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.BDDMockito.given;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@RunWith(MockitoJUnitRunner.class)
-public class EventApiTest {
-    @InjectMocks
-    private EventApi controller;
 
-    @Mock
-    private EventAddService addService;
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class EventAddServiceTest {
 
-    private ReviewRepository reviewRepository;
-
-    private ObjectMapper objectMapper;
-
+    @Autowired
     private MockMvc mockMvc;
 
-    @Before
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(controller)
-                .setControllerAdvice(new ExceptionApi())
-                .build();
-    }
+    @Autowired
+    private EventAddService addService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    public void 리뷰작성() throws Exception {
-        final EventActionRequest dto = EventActionRequest.builder()
-                .userId("test")
-                .content("content")
-                .reviewId("review Id")
+    public void 리뷰작성_성공() throws Exception {
+        EventActionRequest dto = buildRequest();
+
+        mockMvc.perform(post("/events")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8)
+                        .content(objectMapper.writeValueAsBytes(dto)))
+                .andDo(print())
+                .andExpect(jsonPath("reviewId").value(dto.getReviewId()))
+                .andExpect(jsonPath("placeId").value(dto.getPlaceId()))
+                ;
+    }
+
+    
+
+
+    private EventActionRequest buildRequest() {
+
+        List<String> photoList = new ArrayList<>();
+        photoList.add("photo1");
+        photoList.add("photo2");
+
+        EventActionRequest dto = EventActionRequest.builder()
+                .userId("testUser")
+                .attachedPhotoIds(photoList)
+                .content("test")
+                .reviewId("testReviewId")
                 .action(ActionType.ADD)
-                .placeId("test")
+                .placeId("tokyo")
                 .type(ReviewType.REVIEW)
                 .build();
 
-        final Events event = Events.builder()
-                .userId("test")
-                .content("content")
-                .reviewId("review Id")
-                .action(ActionType.ADD)
-                .placeId("test")
-                .type(ReviewType.REVIEW)
-                .build();
+        return dto;
+    }
 
-        given(addService.handleAction(dto)).willReturn(event);
-
-        ResultActions resultActions = mockMvc.perform(post("/events")
-            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(objectMapper.writeValueAsString(dto)))
-                .andDo(print());
+    private void saveEvents() {
+        EventActionRequest dto = buildRequest();
+        addService.handleAction(dto);
     }
 }
